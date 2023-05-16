303c1688f82e63aaf1dbed8700b40fe759846fde
Arturo-Castro <castro.ah91@gmail.com>
2023-05-16 20:24:50 -0300

    Add migration
    Adds isDeleted and deletedAt attributes to person entity for the soft delete method

--- PeopleManagerApp/PeopleManagerApp.Domain/Entities/Person.cs
+++ PeopleManagerApp/PeopleManagerApp.Domain/Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -19,5 +20,8 @@ namespace PeopleManagerApp.Domain.Entities
         public string Address { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public string Email { get; set; } = null!;
+        [DefaultValue("false")]
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }
