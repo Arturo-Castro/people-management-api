8d82e7525592e3199cc0d75f7383accef04a3fbf
Arturo-Castro <castro.ah91@gmail.com>
2023-05-17 12:53:24 -0300

    Add tests for people controller

---
+++ PeopleManagerApp/PeopleManagerApp.TestApi/MockData/PeopleMockData.cs
@@ -0,0 +1,65 @@
+﻿using PeopleManagerApp.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManagerApp.TestApi.MockData
+{
+    public class PeopleMockData
+    {
+        public static List<PersonDto> GetPeople()
+        {
+            return new List<PersonDto>
+            {
+                new PersonDto
+                {
+                    Id = 1,
+                    FullName = "test 1",
+                    Age = 22,
+                    Address = "TestAddress1",
+                    Email = "test1@yahoo.com"                    
+                },
+                new PersonDto
+                {
+                    Id = 2,
+                    FullName = "test 2",
+                    Age = 32,
+                    Address = "TestAddress2",
+                    Email = "test2@yahoo.com"                    
+                },
+                new PersonDto
+                {
+                    Id = 3,
+                    FullName = "test 3",
+                    Age = 22,
+                    Address = "TestAddress3",
+                    Email = "test3@yahoo.com"                    
+                }
+            };
+        }
+
+        public static List<PersonDto> EmptyPersonDtoList()
+        {
+            return new List<PersonDto>();
+        }
+
+        public static PersonDto GetPersonDto(long id)
+        {
+            return new PersonDto 
+            {
+                Id = id,
+                FullName = "test 4",
+                Age = 15,
+                Address = "TestAddress4",
+                Email = "test4@yahoo.com"
+            };
+        }
+
+        public static PersonDto GetNullResponse()
+        {
+            return null;
+        }
+    }
+}
--- PeopleManagerApp/PeopleManagerApp.TestApi/Systems/Controllers/TestPeopleController.cs
+++ PeopleManagerApp/PeopleManagerApp.TestApi/Systems/Controllers/TestPeopleController.cs
@@ -1,12 +1,137 @@
-﻿using System;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PeopleManagerApp.Api.Controllers;
+using PeopleManagerApp.Application.Interfaces;
+using PeopleManagerApp.TestApi.MockData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace PeopleManagerApp.TestApi.Systems.Controllers
-{
+{    
     public class TestPeopleController
     {
+        private readonly Mock<IPersonService> _personServiceMock;
+        private readonly PeopleController _sut;        
+
+        public TestPeopleController()
+        {
+            this._personServiceMock = new Mock<IPersonService>();
+            this._sut = new PeopleController(_personServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAllPeople_ShouldReturn200Status()
+        {
+            //Arrange            
+            this._personServiceMock.Setup(_ => _.GetAllPeople()).ReturnsAsync(PeopleMockData.GetPeople());           
+
+            //Act
+            var result = await this._sut.GetAllPeople();
+
+            //Assert
+            result.GetType().Should().Be(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task GetAllPeople_ShouldReturn404Status()
+        {
+            //Arrange            
+            this._personServiceMock.Setup(_ => _.GetAllPeople()).ReturnsAsync(PeopleMockData.EmptyPersonDtoList());
+
+            //Act
+            var result = await this._sut.GetAllPeople();
+
+            //Assert
+            result.GetType().Should().Be(typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public async Task GetPersonById_ShouldReturn200Status()
+        {
+            //Arrange
+            var expectedPersonId = 3;
+            this._personServiceMock.Setup(_ => _.GetPersonById(expectedPersonId)).ReturnsAsync(PeopleMockData.GetPersonDto(expectedPersonId));
+
+            //Act
+            var result = await this._sut.GetPersonById(expectedPersonId);
+
+            //Assert
+            result.GetType().Should().Be(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task GetPersonById_ShouldReturn404Status()
+        {
+            //Arrange
+            var expectedPersonId = 6;
+            this._personServiceMock.Setup(_ => _.GetPersonById(expectedPersonId)).ReturnsAsync(PeopleMockData.GetNullResponse());
+
+            //Act
+            var result = await this._sut.GetPersonById(expectedPersonId);
+
+            //Assert
+            result.GetType().Should().Be(typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public async Task GetRandomPerson_ShouldReturn200Status()
+        {
+            //Arrange
+            var random = new Random();
+            var randomSkip = random.Next(0, 3);
+            this._personServiceMock.Setup(_ => _.GetRandomPerson()).ReturnsAsync(PeopleMockData.GetPeople().Skip(randomSkip).First());
+
+            //Act
+            var result = await this._sut.GetRandomPerson();
+
+            //Assert
+            result.GetType().Should().Be(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task GetRandomPerson_ShouldReturn404Status()
+        {
+            //Arrange            
+            this._personServiceMock.Setup(_ => _.GetRandomPerson()).ReturnsAsync(PeopleMockData.GetNullResponse());
+
+            //Act
+            var result = await this._sut.GetRandomPerson();
+
+            //Assert
+            result.GetType().Should().Be(typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public async Task SoftDeletePerson_ShouldReturn200Status()
+        {
+            //Arrange
+            var expectedPersonId = 6;
+            this._personServiceMock.Setup(_ => _.SoftDeletePerson(expectedPersonId)).ReturnsAsync(true);
+
+            //Act
+            var result = await this._sut.SoftDeletePerson(expectedPersonId);
+
+            //Assert
+            result.GetType().Should().Be(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task SoftDeletePerson_ShouldReturn404Status()
+        {
+            //Arrange
+            var expectedPersonId = 6;
+            this._personServiceMock.Setup(_ => _.SoftDeletePerson(expectedPersonId)).ReturnsAsync(false);
+
+            //Act
+            var result = await this._sut.SoftDeletePerson(expectedPersonId);
+
+            //Assert
+            result.GetType().Should().Be(typeof(NotFoundResult));
+        }
     }
 }
